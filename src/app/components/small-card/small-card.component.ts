ab1663e1938c0ba89e02f2a265db4937c4790b18
DFavacho <dfavacho@vsb.net.br>
2023-06-11 23:00:05 -0300

    Adiciona content e finaliza o projeto

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit, Input } from '@angular/core';
 export class SmallCardComponent implements OnInit {
 
   @Input()
+  id:string= "0";
+  @Input()
   photoCover:string = "";
   @Input()
   cardTitle:string = "";
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "photo": "https://geekblog.com.br/wp-content/uploads/2022/11/homem-de-ferro-12-950x500.jpg.webp",
+        "title": "NOVO HOMEM DE FERRO ANUNCIADO",
+        "description": "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos."
+    },
+    {
+        "id":"2",
+        "photo": "https://criticalhits.com.br/wp-content/uploads/2022/08/1140437.jpg",
+        "title": "Homem de ferro com nova estrutura",
+        "description": "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos."
+    }
+]
